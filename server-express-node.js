fd32c4c962c7e38fa019d9b81071e77a8e9fa134
StefKas1 <85234116+StefKas1@users.noreply.github.com>
2021-11-26 14:20:06 +0100

    Initial commit.

---
+++ server-express-node.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+// Either Heroku sets port or port 3000 is used.
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
